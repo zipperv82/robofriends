8e3d9c1daf3ea6c1beb8d6d3fcd41cebe48fa447
zipperv82 <zipperv82@gmail.com>
2020-10-05 11:33:18 -0400

    215 build react app 4

---
+++ src/SearchBox.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import 'tachyons';
+
+
+const SearchBox = ({ searchChange }) => {
+    return (
+        <div  className='pa2'>
+        <input
+                className='pa3 ba b--green bg-lightest-blue'
+                type='search'
+                placeholder='search'
+                onChange={searchChange}
+            />
+        </div>
+        );
+    }
+
+    export default SearchBox;
